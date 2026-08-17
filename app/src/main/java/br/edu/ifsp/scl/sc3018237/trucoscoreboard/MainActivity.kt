c57cf0e8e881bbe1d58e0f78856d088aa5d41993
Fernnado <Ferdinhokirei666@gmail.com>
2026-08-16 21:19:46 -0300

    feat:adicionar placar da equipe A

---
+++ app/src/main/java/br/edu/ifsp/scl/sc3018237/trucoscoreboard/MainActivity.kt
@@ -0,0 +1,16 @@
+package br.edu.ifsp.scl.sc3018237.trucoscoreboard
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+    }
+}
